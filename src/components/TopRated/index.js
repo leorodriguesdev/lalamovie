89005ef25ec4715a63ca87e1dc117a2f35a150a9
Leonardo Rodrigues <leonardo.rodrigues@ibm.com>
2021-12-09 18:40:51 -0300

    first attempt on the Coca challenge with callback

--- src/components/TopRated/index.js
+++ src/components/TopRated/index.js
@@ -1,43 +1,53 @@
-import React , {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 import movieList from "../../services/movieList";
 import { Card } from "../Card";
 
 
+const getMovieList = () => {
+
+    return movieList.map(movie => {
+
+        return { ...movie, key: movie.id };
+})};
 
 
-export function TopRated( props ) {
 
-    // const [movies,setMovies] = useState(undefined);
 
-    // const metodo = (callback)=>{
-    //     movies = buscarListaDeFilmes();
-    //     callback(movies);
-    // }
-    // // useEffect(()=>{
-    // //     //import o metodo que lista os top rateds
-    // //     //chamar o método e definir um meio para que esse método atuaklize o estado
-        
-    // //     // setMovies([]);
-    // //     metodo((movies)=>{
-    // //         setMovies(movies);
-    // //     })
-    // // },[]);
 
-    
+     export function TopRated(props) {
 
-    const { navigate } = props;
+        const [movies, setMovies] = useState(getMovieList);
 
-    const meuNavigationToMovie = () => {
-        navigate("Movie")
-    }   
+        const metodo = (callback) => {
+            movies = getMovieList();
+            callback(movies);
+         }
+        useEffect(() => {
+            // import o metodo que lista os top rateds
+            // chamar o método e definir um meio para que esse método atuaklize o estado
 
-    const meuRenderItem = ({item}) => (
-        <Card movie={item} navigationToMovie={meuNavigationToMovie}/>
-    )
-    
-    
-    return (
+               console.warn("mudou");
+
+              setMovies([]);
+            //  metodo((movies) => {
+            //      setMovies(movies);
+            //  })
+        }, [movies]);
+
+
+        const { navigate } = props;
+
+        const meuNavigationToMovie = () => {
+            navigate("Movie")
+        }
+
+        const meuRenderItem = ({ item }) => (
+            <Card movie={item} navigationToMovie={meuNavigationToMovie} />
+        )
+
+
+        return (
             <View >
                 <Text style={styles.title}>
                     <Text style={styles.bar}>|</Text>  Top Rated
@@ -52,28 +62,28 @@ export function TopRated( props ) {
                     keyExtractor={(item) => item.id}
                     horizontal
                     renderItem={meuRenderItem}
-                    
+
                 >
                 </FlatList>
 
             </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    title: {
-        fontSize: 25,
-        color: "#fff",      
-        fontWeight: "bold",
-        marginVertical: 10,
-        
-    },
-    bar: {
-        color: "#FDD030",
-    },  
-    card: {
-        marginHorizontal: 10,
+        )
     }
 
+    const styles = StyleSheet.create({
+        title: {
+            fontSize: 25,
+            color: "#fff",
+            fontWeight: "bold",
+            marginVertical: 10,
 
-});
+        },
+        bar: {
+            color: "#FDD030",
+        },
+        card: {
+            marginHorizontal: 10,
+        }
+
+
+    });
